e8121f45c6c424c031aa558817b0c81227995108
Gowtham Dongari <gowthamdongari@gmail.com>
2024-02-04 14:21:57 -0600

    initial sidenavbarsetup

--- frontend/src/components/SideNavBar.jsx
+++ frontend/src/components/SideNavBar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 
+const styles = {
+  Card: {
+    top: '89px',
+    left: '0px',
+    width: '264px',
+    height: '1532px',
+    backgroundColor: '#161616',
+    border: '1px solid #282828',
+    boxSizing: 'border-box',
+  },
+};
+
 const SideNavBar = () => {
   return (
-    <nav>
-        sidenavbar
-    </nav>
+     <>
+      
+     </>
   )
 }
 
 export default SideNavBar
+
+
+
+
+
+
+
+
+
+
