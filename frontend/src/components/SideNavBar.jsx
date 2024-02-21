4c1c962b38fcd892e67799afcefdb527ed4e1ae5
Ramaruva <ramaruva54@gmail.com>
2024-02-21 13:05:28 -0600

    validation demo setup

--- frontend/src/components/SideNavBar.jsx
+++ frontend/src/components/SideNavBar.jsx
@@ -1,93 +1,125 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-const SideNavBar = () => {
-  // write all the links here
-
-  const staffLinks = [
-    {
-      title: "Professional Reviews",
-      link: "/home/professionalReviews",
-    },
-    {
-      title: "Empolyer Reviews",
-      link: "/home/empolyerReviews",
-    },
-    {
-      title: "Professional lists",
-      link: "/home/ProfessionalLists",
-    },
-    {
-      title: "Empolyer Lists",
-      link: "/home/empolyerLists",
-    },
-    {
-      title: "Notifications",
-      link: "/home/notifications",
-    },
-    {
-      title: "Settings",
-      link: "/home/Settings",
-    },
-  ];
-
-  const employerLinks = [
-    {
-      title: "Create Job",
-      link: "/home/CreateJobs",
-    },
-    {
-      title: "Job Lists",
-      link: "/home/JobLists",
-    },
-    {
-      title: "Notifications",
-      link: "/home/notifications?user=empolyer",
-    },
-    {
-      title:"Payments",
-      link:"/home/payments"
-    },
-    {
-      title:"Settings",
-      link:"/home/Settings?user=empolyer"
-    }
-  ];
-  const ProfessionalLinks = [
-    {
-      title: "Browse Jobs",
-      link: "/home/BrowseJobs",
-    },
-    {
-      title: "Matched Jobs",
-      link: "/home/MatchedJobs",
-    },
-    {
-      title: "Payments",
-      link: "/home/Payments",
-    },
-    {
-      title: "Settings",
-      link: "/home/Settings?user=professional",
-    },
-  ];
-
- const rootLinks =[
+const staffLinks = [
   {
-     title:"Create Staff Accounts",
-     link:"/home/createAccount"
+    title: "Professional Reviews",
+    link: "/home/professionalReviews",
   },
   {
-    title:"Staff Accounts",
-    link:"/home/staffAccounts"
-  }
- ]
+    title: "Empolyer Reviews",
+    link: "/home/empolyerReviews",
+  },
+  {
+    title: "Professional lists",
+    link: "/home/ProfessionalLists",
+  },
+  {
+    title: "Empolyer Lists",
+    link: "/home/empolyerLists",
+  },
+  {
+    title: "Notifications",
+    link: "/home/notifications",
+  },
+  {
+    title: "Settings",
+    link: "/home/Settings",
+  },
+];
+
+const employerLinks = [
+  {
+    title: "Create Job",
+    link: "/home/CreateJobs",
+  },
+  {
+    title: "Job Lists",
+    link: "/home/JobLists",
+  },
+  {
+    title: "Notifications",
+    link: "/home/notifications?user=empolyer",
+  },
+  {
+    title: "Payments",
+    link: "/home/payments",
+  },
+  {
+    title: "Settings",
+    link: "/home/Settings?user=empolyer",
+  },
+];
+const ProfessionalLinks = [
+  {
+    title: "Browse Jobs",
+    link: "/home/BrowseJobs",
+  },
+  {
+    title: "Matched Jobs",
+    link: "/home/MatchedJobs",
+  },
+  {
+    title: "Payments",
+    link: "/home/Payments",
+  },
+  {
+    title: "Settings",
+    link: "/home/Settings?user=professional",
+  },
+];
+
+const rootLinks = [
+  {
+    title: "Create Staff Accounts",
+    link: "/home/createAccount",
+  },
+  {
+    title: "Staff Accounts",
+    link: "/home/staffAccounts",
+  },
+];
+const SideNavBar = () => {
+  // write all the links here
+  const [links, setLinks] = useState(rootLinks);
+  const handleLinks = (val) => {
+    try {
+      switch (val) {
+        case "Professional":
+          setLinks(ProfessionalLinks);
+          break;
+        case "Empolyer":
+          setLinks(employerLinks);
+          break;
+        case "staff":
+          setLinks(staffLinks);
+          break;
+        case "root":
+          setLinks(rootLinks);
+          break;
+        default:
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      const userDetails = JSON.parse(localStorage.getItem("user"));
+      handleLinks(userDetails?.role);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <aside>
       <div className="w-48 h-screen mt-2 ml-3 bg-gray-900 p-4 space-y-2">
         {/* Navigation links */}
         <nav>
           {/* Repeat this structure for each menu item */}
-          {ProfessionalLinks.map((item,index) => {
+          {links.map((item, index) => {
             return (
               <NavLink
                 to={item.link}
--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,12 +1,21 @@
-
-import { Menu } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import SignIn from "../auth/SignIn";
- 
-const TopNavBar = ({ isLandingPage = false,isSignin=false }) => {
+
+const TopNavBar = ({ isLandingPage = false, isSignin = false }) => {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState({
+    userName: "gowtham123",
+    role: "staff",
+  });
+  useEffect(()=>{
+    try {
+      const userDetails1 = JSON.parse(localStorage.getItem("user"));
+      setUserDetails(userDetails1);
+    } catch (error) {
+      console.log(error);
+    }
+  },[])
   return (
     <div className="bg-white space-y-4  rounded-md sm:px-5 sm:py-3 lg:p-3 xl:px-6 xl:py-4">
       <div className="flex items-center justify-between border-b">
@@ -19,38 +28,40 @@ const TopNavBar = ({ isLandingPage = false,isSignin=false }) => {
         </div>
         <div className="flex flex-row justify-center ">
           <div className="m-1">
-           { !isLandingPage &&<div className="relative">
-              <img
-                src="/assets/Profile-pic.jpg" // Replace this with your profile picture URL
-                alt="Profile"
-                className="w-8 h-8 mr-4 rounded-full cursor-pointer"
-                onClick={() => setIsOpen(!isOpen)}
-              />
-              {isOpen && (
-                <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md py-1">
-                  <NavLink
+            {!isLandingPage && (
+              <div className="relative">
+                <img
+                  src="/assets/Profile-pic.jpg" // Replace this with your profile picture URL
+                  alt="Profile"
+                  className="w-8 h-8 mr-4 rounded-full cursor-pointer"
                   onClick={() => setIsOpen(!isOpen)}
-                    to="/home/PasswordChange" // Replace this with your actual link to account settings
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Change Password
-                  </NavLink>
-                  <NavLink
-                    to="/SignIn" // Implement your logout functionality here
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Logout
-                  </NavLink>
-                </div>
-              )}
-            </div>}
+                />
+                {isOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-md py-1">
+                    <NavLink
+                      onClick={() => setIsOpen(!isOpen)}
+                      to="/home/PasswordChange" // Replace this with your actual link to account settings
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Change Password
+                    </NavLink>
+                    <NavLink
+                      to="/SignIn" // Implement your logout functionality here
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Logout
+                    </NavLink>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
           {isLandingPage ? (
-            <button onClick={()=>navigate("/SignIn")}>Login</button>
+            <button onClick={() => navigate("/SignIn")}>Login</button>
           ) : (
             <div className="ml-1">
-              <p className="text-sm font-semibold">Professional</p>
-              <div className="text-xs text-gray-600">Developer</div>
+              <p className="text-sm font-semibold">{userDetails.userName}</p>
+              <div className="text-xs text-gray-600">{userDetails.role}</div>
             </div>
           )}
         </div>
@@ -58,6 +69,5 @@ const TopNavBar = ({ isLandingPage = false,isSignin=false }) => {
     </div>
   );
 };
- 
+
 export default TopNavBar;
- 
